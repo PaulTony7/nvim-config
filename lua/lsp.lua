21278abbb5869e76fa44f99051a84dbfd3d70b7d
Paul <PaulKokot98@gmail.com>
2025-03-27 19:22:12 +0100

    C from linux lsp

--- lua/lsp.lua
+++ lua/lsp.lua
@@ -35,7 +35,8 @@ require('lspconfig').csharp_ls.setup(
 		handlers = {
     ["textDocument/definition"] = require('csharpls_extended').handler,
     ["textDocument/typeDefinition"] = require('csharpls_extended').handler,
-  },	
+        },	
 	}
 )
+require('lspconfig').clangd.setup{}
 
