bb04a4d67adaba209ebf8f317599c12ee7753749
Paul <PaulKokot98@gmail.com>
2025-04-17 19:32:39 +0200

    Cleanup config

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -1,3 +1,43 @@
+local Plug = vim.fn['plug#']
+
+-- https://github.com/junegunn/vim-plug
+vim.call('plug#begin')
+
+-- theme
+Plug 'navarasu/onedark.nvim'
+
+-- lsp configurations
+Plug 'neovim/nvim-lspconfig'
+-- autocomplition
+Plug 'hrsh7th/nvim-cmp'
+Plug 'hrsh7th/cmp-nvim-lsp'
+
+Plug 'nvim-lua/plenary.nvim'
+
+Plug 'nvim-treesitter/nvim-treesitter'
+
+Plug 'nvim-telescope/telescope.nvim'
+
+Plug 'Decodetalkers/csharpls-extended-lsp.nvim'
+
+Plug 'tikhomirov/vim-glsl'
+
+Plug 'sindrets/diffview.nvim'
+
+-- neo-tree
+Plug "MunifTanjim/nui.nvim"
+Plug "nvim-tree/nvim-web-devicons"
+Plug 'nvim-neo-tree/neo-tree.nvim'
+
+vim.call('plug#end')
+
 require('plugins/telescope')
 require('plugins/diffview')
 require('plugins/neotree')
+
+require('onedark').setup {
+    style = 'warm'
+}
+require('onedark').load()
+
+require('lsp')
